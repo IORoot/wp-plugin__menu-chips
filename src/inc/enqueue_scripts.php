459622cd93ccfd91f021b9bb248c420a1ae08c78
Andy Pearson <andy@londonparkour.com>
2020-07-27 08:02:18 +0100

    Enqueue only on pulse pages

--- src/inc/enqueue_scripts.php
+++ src/inc/enqueue_scripts.php
@@ -6,12 +6,17 @@
  */
 function register_chips() {
     
-    // Load and enqueue now, before everything else.
-    wp_register_style( 'andyp_chips_css', '/wp-content/plugins/andyp_pulse_chips/src/sass/style.css' );
-    wp_enqueue_style( 'andyp_chips_css' );
 
-    wp_register_script( 'andyp_chips_js', '/wp-content/plugins/andyp_pulse_chips/src/js/current_page.js' );
-    wp_enqueue_script('andyp_chips_js');
+    if (is_page('pulse') || is_page('ig') || is_page('youtube') || is_page('podcasts')) {
+
+        // Load and enqueue now, before everything else.
+        wp_register_style('andyp_chips_css', '/wp-content/plugins/andyp_pulse_chips/src/sass/style.css');
+        wp_enqueue_style('andyp_chips_css');
+
+        wp_register_script('andyp_chips_js', '/wp-content/plugins/andyp_pulse_chips/src/js/current_page.js');
+        wp_enqueue_script('andyp_chips_js');
+
+    }
 
 }
 
